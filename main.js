e64d523c6990a9337a4cbd6a7f0b8bd9255addc8
Dorota Pindur <dpindur@wp.pl>
2022-04-28 19:32:58 +0200

    add style and js to header

---
+++ main.js
@@ -0,0 +1,16 @@
+const menuButton = document.querySelector('.menu-button--js');
+const menu = document.querySelector('.menu');
+let isOpen = false;
+
+menuButton.addEventListener('click', () => {
+    if (isOpen) {
+        menu.classList.remove('menu-open');
+        menuButton.innerHTML = '<img class="menu-img" src="./open_menu.svg" alt="close menu">';
+        isOpen = false;
+    } else {
+        menu.classList.add('menu-open');
+        menuButton.innerHTML = '<img class="menu-img" src="./close_menu.svg" alt="open menu">';
+        isOpen = true;
+    }
+});
+
