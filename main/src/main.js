1eeca1278fdf11ebf707da501372d0641fbe2f04
Katarzyna Kosiak <kate2805@op.pl>
2024-09-12 19:35:53 +0200

    fix burger

--- main/src/main.js
+++ main/src/main.js
@@ -1,3 +1,18 @@
+const menuButton = document.querySelector('.menu-button--js');
+const menu = document.querySelector('.menu');
+let isOpen = false;
+
+menuButton.addEventListener('click', () => {
+    if (isOpen) {
+        menu.classList.remove('menu-open');
+        menuButton.innerHTML = '<img class="menu-img" src="main/images/pagegraphic/open_menu.svg" alt="close menu.">';
+        isOpen = false;
+    } else {
+        menu.classList.add('menu-open');
+        menuButton.innerHTML = '<img class="menu-img" src="main/images/pagegraphic/close_menu.svg" alt="open menu.">';
+        isOpen = true;
+    }
+});
 
 
 let slideIndex = 1;
